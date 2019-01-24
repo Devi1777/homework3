a1ffa9ecb8d827c9925ddccc5c55918b216ac541
Devi1777 <45236634+Devi1777@users.noreply.github.com>
2019-01-24 21:42:00 +0800

    提交作业2

---
+++ LevelTraversal.java
@@ -0,0 +1,50 @@
+/**
+ * author: rale
+ * createdAt: 1/20/19
+ * 对树节点的水平遍历
+ * 难度：HARD
+ */
+public class LevelTraversal {
+
+    /**
+     *  对传入的树的根节点进行水平遍历
+     *  如树的结构为：
+     *      a
+     *     / \
+     *    b   c
+     *    则返回的字符串为abc
+     *
+     *    涉及知识点：
+     *    1.水平遍历（Level Traversal)
+     *    2.StringBuilder与String
+     *    3.队列（Queue）
+     */
+    public String traverse(TreeNode root) {
+        return "";
+        if ( root == null ) {
+            return "";
+        }
+        Queue <TreeNode> queue = new LinkedList <TreeNode>();
+        queue.offer(root);
+
+        StringBuilder result = new StringBuilder();
+        While (!queue.isEmpty()){
+            TreeNode current = queue.poll();
+            result.append(current.getletter());
+
+            if (current.getLeftChild()!= null){
+                queue.offer(current.getLeftChild);
+            }
+
+            if (current.getRightChild()!= null){
+                queue.offer(current.getRightChild);
+            }
+        }
+        return result.toString();
+    }
+
+
+
+
+
+}
